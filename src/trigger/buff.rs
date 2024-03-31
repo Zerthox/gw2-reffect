492a905a1f5351c5f456699ab2d7e7e97f77b979
Zerthox <zerthox@gmail.com>
2024-03-31 01:39:25 +0100

    Add not trigger

--- src/trigger/buff.rs
+++ src/trigger/buff.rs
@@ -5,6 +5,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub enum BuffTrigger {
     Single(u32),
+    Not(Box<BuffTrigger>),
     Any(Vec<u32>),
     All(Vec<u32>),
 }
@@ -16,6 +17,7 @@ impl BuffTrigger {
         } else {
             match self {
                 Self::Single(id) => ctx.stacks_of(*id),
+                Self::Not(_) => Some(0),
                 Self::Any(ids) => ids.iter().copied().find_map(|id| ctx.stacks_of(id)),
                 Self::All(ids) => ids.iter().copied().find_map(|id| ctx.stacks_of(id)),
             }
@@ -28,6 +30,7 @@ impl Trigger for BuffTrigger {
         ctx.edit
             || match self {
                 Self::Single(id) => ctx.has_buff(*id),
+                Self::Not(inner) => !inner.is_active(ctx),
                 Self::Any(ids) => ids.iter().copied().any(|buff| ctx.has_buff(buff)),
                 Self::All(ids) => ids.iter().copied().all(|buff| ctx.has_buff(buff)),
             }
