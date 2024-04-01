e2880edb660508ee0c8b26f3df0652f69253a701
Zerthox <zerthox@gmail.com>
2024-04-01 18:53:15 +0200

    Adjust default text position

--- src/element/text_align.rs
+++ src/element/text_align.rs
@@ -2,6 +2,8 @@ use nexus::imgui::{Selectable, Ui};
 use serde::{Deserialize, Serialize};
 use strum::{AsRefStr, EnumIter, IntoEnumIterator};
 
+// TODO: vertical align?
+
 #[derive(
     Debug,
     Clone,
@@ -25,10 +27,12 @@ pub enum TextAlign {
 impl TextAlign {
     pub fn calc_pos(&self, ui: &Ui, text: impl AsRef<str>) -> [f32; 2] {
         let [text_x, _] = ui.calc_text_size(text);
+        let line_height = ui.text_line_height();
+        let offset_y = -0.5 * line_height;
         match self {
-            TextAlign::Left => [0.0, 0.0],
-            TextAlign::Center => [-0.5 * text_x, 0.0],
-            TextAlign::Right => [-text_x, 0.0],
+            TextAlign::Left => [0.0, offset_y],
+            TextAlign::Center => [-0.5 * text_x, offset_y],
+            TextAlign::Right => [-text_x, offset_y],
         }
     }
 
