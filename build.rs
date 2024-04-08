bb4f40a450e8967608616eee630e10720dba3c18
Zerthox <zerthox@gmail.com>
2024-04-08 22:59:53 +0200

    Adjust build script

--- build.rs
+++ build.rs
@@ -6,12 +6,12 @@ fn main() {
     let manifest = env::var_os("CARGO_MANIFEST_DIR").unwrap();
     let profile = env::var("PROFILE").unwrap();
 
-    // target path
     let target = Path::new(&manifest).join("target").join(profile);
 
     let deps = Path::new(&manifest).join(DEPS_PATH);
     for entry in deps.read_dir().unwrap() {
         let entry = entry.unwrap();
+        println!("cargo:rerun-if-changed={}", entry.path().display());
         if entry.metadata().unwrap().is_file() {
             fs::copy(entry.path(), target.join(entry.file_name())).unwrap();
         }
