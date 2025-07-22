14271b2ccea3aad687299a294a5afe12915ce319
Zerthox <zerthox@gmail.com>
2025-07-22 20:00:40 +0200

    Add shake & bounce animations

--- reffect/src/elements/animation.rs
+++ reffect/src/elements/animation.rs
@@ -1,11 +1,14 @@
 use crate::{
     elements::RenderCtx,
-    render::{cycle_progress, enum_combo, helper, input_seconds, push_alpha_change},
+    render::{
+        cycle_progress, enum_combo, helper, input_float_with_format, input_seconds,
+        push_alpha_change,
+    },
     util::non_zero_u32,
 };
 use const_default::ConstDefault;
-use nexus::imgui::{ComboBoxFlags, StyleStackToken, Ui};
-
+use nexus::imgui::{ComboBoxFlags, InputTextFlags, StyleStackToken, Ui};
+use reffect_core::enums::check_variant_array;
 use serde::{Deserialize, Serialize};
 use std::num::NonZero;
 use strum::{AsRefStr, EnumCount, EnumIter, IntoStaticStr, VariantArray};
@@ -30,7 +33,7 @@ impl Animation {
 impl ConstDefault for Animation {
     const DEFAULT: Self = Self {
         kind: AnimationKind::Pulse,
-        period: non_zero_u32!(3000),
+        period: non_zero_u32!(1000),
     };
 }
 
@@ -49,16 +52,28 @@ impl Animation {
             self.period = NonZero::new(period).unwrap_or(non_zero_u32!(1));
         }
         helper(ui, || ui.text("Animation loop duration in seconds"));
+
+        self.kind.render_options(ui);
     }
 }
 
 // TODO: tint animation changing color via state?
-#[derive(
-    Debug, Clone, AsRefStr, IntoStaticStr, EnumIter, EnumCount, VariantArray, Serialize, Deserialize,
-)]
+#[derive(Debug, Clone, AsRefStr, IntoStaticStr, EnumIter, EnumCount, Serialize, Deserialize)]
 pub enum AnimationKind {
     Pulse,
+    Shake { intensity: f32 },
+    Bounce { intensity: f32 },
 }
+
+impl VariantArray for AnimationKind {
+    const VARIANTS: &'static [Self] = &[
+        Self::Pulse,
+        Self::Shake { intensity: 10.0 },
+        Self::Bounce { intensity: 10.0 },
+    ];
+}
+
+const _: () = check_variant_array::<AnimationKind>();
 
 impl AnimationKind {
     fn back_and_forth(progress: f32) -> f32 {
@@ -69,13 +84,9 @@ impl AnimationKind {
         }
     }
 
-    pub fn animate<'ui>(
-        &mut self,
-        ui: &'ui Ui,
-        _ctx: &RenderCtx,
-        progress: f32,
-    ) -> impl Drop + 'ui {
+    pub fn animate<'ui>(&mut self, ui: &'ui Ui, ctx: &RenderCtx, progress: f32) -> impl Drop + 'ui {
         enum Token<'ui> {
+            None,
             Style(StyleStackToken<'ui>),
         }
 
@@ -89,6 +100,32 @@ impl AnimationKind {
                 // TODO: dont disappear completely?
                 let progress = Self::back_and_forth(progress);
                 Token::Style(push_alpha_change(ui, progress))
+            }
+            Self::Shake { intensity } => {
+                let progress = Self::back_and_forth(progress);
+                ctx.add_offset([(progress - 0.5) * intensity, 0.0]);
+                Token::None
+            }
+            Self::Bounce { intensity } => {
+                let progress = Self::back_and_forth(progress);
+                ctx.add_offset([0.0, (progress - 0.5) * intensity]);
+                Token::None
+            }
+        }
+    }
+
+    pub fn render_options(&mut self, _ui: &Ui) {
+        match self {
+            AnimationKind::Pulse => {}
+            AnimationKind::Shake { intensity } | AnimationKind::Bounce { intensity } => {
+                input_float_with_format(
+                    "Intensity",
+                    intensity,
+                    1.0,
+                    10.0,
+                    "%.1f",
+                    InputTextFlags::empty(),
+                );
             }
         }
     }
