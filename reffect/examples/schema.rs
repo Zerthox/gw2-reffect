56f474a32d8393eff36495554f8d211c46795ce8
Zerthox <zerthox@gmail.com>
2026-07-10 20:37:45 +0200

    Update schema

--- reffect/examples/schema.rs
+++ reffect/examples/schema.rs
@@ -1,11 +1,12 @@
 use reffect::schema::Schema;
 use schemars::schema_for;
-use std::{fs::File, io::BufWriter};
+use std::{fs::File, io::BufWriter, path::PathBuf};
 
-const OUT: &str = "schema.json";
+const MANIFEST: &str = env!("CARGO_MANIFEST_DIR");
 
 fn main() {
-    let file = File::create(OUT).expect("failed to open file");
+    let path = PathBuf::from(MANIFEST).join("../docs/schema.json");
+    let file = File::create(path).expect("failed to open output file");
     let schema = schema_for!(Schema);
     serde_json::to_writer_pretty(BufWriter::new(file), &schema).expect("failed to output");
 }
